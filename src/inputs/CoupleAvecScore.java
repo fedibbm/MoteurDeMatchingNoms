66603d241a9c8f8306a87bd6346b8cec257e798d
fedibbm <benbrahimfedi4@gmail.com>
2025-05-09 15:29:08 +0100

    update CoupleAvecScore class

--- src/inputs/CoupleAvecScore.java
+++ src/inputs/CoupleAvecScore.java
@@ -1,4 +1,13 @@
 package inputs;
+//
+//public record CoupleAvecScore(Couple coupleDeNoms, double score) {
+//}
 
-public record CoupleAvecScore(Couple coupleDeNoms, double score) {
+public class CoupleAvecScore implements Comparable<CoupleAvecScore>{
+    Couple coupleDeNoms;
+    double score;
+
+    public int compareTo(CoupleAvecScore autreCoupleAvecScore){
+        return Double.compare(this.score, autreCoupleAvecScore.score);
+    }
 }
